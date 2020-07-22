e245e0751159a44deb33ed1f971305cc391a1208
ptrias <ptrias@csumb.edu>
2020-07-22 00:20:04 +0000

    lab4

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req,res){
+    res.render("index.html");
+});
+
+app.get("/manifesto", function(req,res){
+    res.render("manifesto.html");
+});
+
+app.get("/principle", function(req,res){
+    res.render("principle.html");
+});
+
+app.get("/scrum", function(req,res){
+    res.render("scrum.html");
+});
+
+app.get("/index", function(req,res){
+    res.render("index.html");
+});
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
